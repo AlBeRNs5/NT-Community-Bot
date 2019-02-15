e39747fd959d37d90beabf585d8da1abd4e1861e
AlBeRNs5 <46230913+AlBeRNs5@users.noreply.github.com>
2019-02-15 22:08:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const prefix = '*'
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`NT `,"https://www.twitch.tv/dggamingbot")
+client.user.setGame(`NT Community - *help `,"https://www.twitch.tv/dggamingbot")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
